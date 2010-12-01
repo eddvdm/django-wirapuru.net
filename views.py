07ef99bfb0afbef0d59736cc908474929511ea2e
unknown <Dudu@.(none)>
2010-12-01 21:21:43 -0200

    woah test #2

--- views.py
+++ views.py
@@ -4,4 +4,4 @@ __author__ = 'Eduardo Vaz de Mello'
 from django.http import HttpResponse
 
 def woah(request):
-    return HttpResponse("<h1>Woah, it works!</h1>") 
+    return HttpResponse("<h1>Woah, it works even without restarting the server!</h1>") 
